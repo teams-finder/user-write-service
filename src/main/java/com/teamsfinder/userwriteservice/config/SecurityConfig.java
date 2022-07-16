47b580717f2da715ea2528a36a56e85f5b91e292
mrrevo55 <kwolny31@gmail.com>
2022-07-16 15:34:51 +0200

    Start implementing security in project

---
+++ src/main/java/com/teamsfinder/userwriteservice/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.teamsfinder.userwriteservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true)
+class SecurityConfig extends GlobalMethodSecurityConfiguration {
+}
