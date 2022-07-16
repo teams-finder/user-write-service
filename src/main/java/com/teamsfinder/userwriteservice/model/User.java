17376d75c654eddf57930137d8f12c992edbca39
mrrevo55 <kwolny31@gmail.com>
2022-07-16 14:14:22 +0200

    Added keycloak id column for user

--- src/main/java/com/teamsfinder/userwriteservice/model/User.java
+++ src/main/java/com/teamsfinder/userwriteservice/model/User.java
@@ -1,21 +1,21 @@
 package com.teamsfinder.userwriteservice.model;
 
+import lombok.Data;
+import lombok.Getter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.UUID;
 
 @Entity
+@Data
 class User {
 
     @Id
-    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
+    private UUID keyCloakId;
 }
