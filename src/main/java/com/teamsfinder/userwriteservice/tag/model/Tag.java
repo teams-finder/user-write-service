2b0fe4dd48f5512418cc272769754a8ac2321648
mrrevo55 <kwolny31@gmail.com>
2022-07-16 18:44:59 +0200

    Removed @Data annotation in Tag.java

--- src/main/java/com/teamsfinder/userwriteservice/tag/model/Tag.java
+++ src/main/java/com/teamsfinder/userwriteservice/tag/model/Tag.java
@@ -1,6 +1,8 @@
 package com.teamsfinder.userwriteservice.tag.model;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,7 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-@Data
+@Getter
+@Setter
 public class Tag {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
