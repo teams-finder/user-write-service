c1066384a0c4063103cbcb4c66c0b7da44f839d1
mrrevo55 <kwolny31@gmail.com>
2022-07-17 14:23:10 +0200

    Added validation in EditUserDto.java

--- src/main/java/com/teamsfinder/userwriteservice/user/dto/EditUserDto.java
+++ src/main/java/com/teamsfinder/userwriteservice/user/dto/EditUserDto.java
@@ -2,8 +2,10 @@ package com.teamsfinder.userwriteservice.user.dto;
 
 import com.teamsfinder.userwriteservice.tag.dto.TagDto;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.UUID;
 
-public record EditUserDto(Long id, String githubProfileUrl, String profilePictureUrl, List<TagDto> tags) {
+public record EditUserDto(@NotNull Long id, @NotEmpty String githubProfileUrl, @NotEmpty String profilePictureUrl, @NotNull List<TagDto> tags) {
 }
