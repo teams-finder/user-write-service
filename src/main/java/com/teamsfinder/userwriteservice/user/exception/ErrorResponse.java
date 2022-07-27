2b8b9e722571aef90434f2295ce7b3425232481e
mrrevo55 <kwolny31@gmail.com>
2022-07-27 19:04:36 +0200

    Changed returns error in ErrorHandler.java

--- src/main/java/com/teamsfinder/userwriteservice/user/exception/ErrorResponse.java
+++ src/main/java/com/teamsfinder/userwriteservice/user/exception/ErrorResponse.java
@@ -2,4 +2,9 @@ package com.teamsfinder.userwriteservice.user.exception;
 
 import java.time.LocalDateTime;
 
-record ErrorResponse(String httpStatus, String message, LocalDateTime localDateTime) { }
+record ErrorResponse(
+        String httpStatus,
+        String message,
+        LocalDateTime localDateTime) {
+    
+}
